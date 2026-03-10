4b21ae8e24315be320ab27b56dad8ae2aadddfd8
Dare Lawal <lawaldare@gmail.com>
2026-03-10 04:57:32 +0000

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,70 +19,14 @@ export class App implements OnInit {
 
   public gridSize = 100;
 
-  public abekePictures = [
-    '1.jpeg',
-    '2.jpeg',
-    '3.jpeg',
-    '4.jpeg',
-    '5.jpeg',
-    '6.jpeg',
-    '7.jpeg',
-    '8.jpeg',
-    '9.jpeg',
-    '10.jpeg',
-  ];
+  private readonly numOfImages = 24;
 
-  public adesewaPictures = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-    '10.jpg',
-    '11.jpg',
-    '12.jpg',
-    '13.jpg',
-    '14.jpg',
-    '15.jpg',
-    '16.jpg',
-    '17.jpg',
-    '18.jpg',
-    '19.jpg',
-    '20.jpg',
-    '21.jpg',
-    '22.jpg',
-    '23.jpg',
-    '24.jpg',
-    '25.jpg',
-    '26.jpg',
-    '27.jpg',
-    '28.jpg',
-    '29.jpg',
-    '30.jpg',
-    '31.jpg',
-    '32.jpg',
-    '33.jpg',
-    '34.jpg',
-    '35.jpg',
-    '36.jpg',
-    '37.jpg',
-    '38.jpg',
-    '39.jpg',
-    '40.jpg',
-    '41.jpg',
-    '42.jpg',
-    '43.jpg',
-    '44.jpg',
-    '45.jpg',
-    '46.jpg',
-    '47.jpg',
-  ];
+  public abekePictures: string[] = [];
 
   ngOnInit(): void {
+    for (let index = 1; index <= this.numOfImages; index++) {
+      this.abekePictures.push(`${index}.jpeg`);
+    }
     const pane = new Pane() as any;
     pane.addBinding(this, 'gridSize', { min: 50, max: 200, step: 1, label: 'Image size' });
   }
